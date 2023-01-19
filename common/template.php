e4bab79141dd82f7efe5a6fcc92b351aad88d3c0
adnene_hamzaoui <adnene.hamzaoui@gmail.com>
2023-01-19 09:59:41 +0100

    fix redirection 404

--- common/template.php
+++ common/template.php
@@ -1,44 +1,40 @@
+<?php
+ob_start()
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <?php
-include 'head.php';
+include 'common/head.php';
 ?>
-
 <body>
     <?php
-
-    include 'header.php';
-    include 'navigation.php';
-
-    if (!isset($page)) {
+    include 'common/header.php';
+    include 'common/navigation.php';
+    if (!isset($page) || empty($page)) {
         $page = 'home';
     }
-
     switch ($page) {
         case 'home':
-            include './content/index_content.php';
+            include 'content/index_content.php';
             break;
         case 'about':
-            include './content/about_content.php';
+            include 'content/about_content.php';
             break;
         case 'products':
-            include './content/products_content.php';
+            include 'content/products_content.php';
             break;
         case 'store':
-            include './content/store_content.php';
+            include 'content/store_content.php';
             break;
         default:
-            header('Location: ./content/404.php');
-            break;
+            header('Location: content/404.php');
+            die();
     }
-    include 'footer.php';
+    include 'common/footer.php';
     ?>
-
     <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> -->
     <!-- Core theme JS-->
     <!-- <script src="js/scripts.js"></script> -->
 </body>
-
 </html>
